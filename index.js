c7b56532f6b9bd235f634177c4ad6575bf97c2fd
Utkarsh <utkarshchoudhary405@gmail.com>
2023-08-20 17:11:41 +0530

    completed one part of the website

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ import bodyParser from "body-parser";
 const app=express();
 const port=3000;
 
+var task=["hello"];
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs",{ task:task});
     // res.send("HELLO");
 })
+
+app.post("/submit",(req,res)=>{
+    var x=req.body["work"];
+    task.push(x);
+    res.render("index.ejs",{ task:task});
+    // res.send("HELLO");
+})
+
 
 app.listen(port,()=>{
     console.log("server is running at server 3000!");
